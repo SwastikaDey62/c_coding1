8bc841dc5dd327536dcf4918d20fc26b2f203183
SwastikaDey62 <deyswastika2020@gmail.com>
2026-04-09 15:33:30 +0530

    Create Stack using for loop.c

---
+++ Stack using for loop.c
@@ -0,0 +1,48 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int a[10]= { };
+int top= -1;
+void push (int val)
+{
+    if (top< (10-1))
+    {
+    top++;
+    a[top]= val;
+} 
+else
+{ 
+    printf ("Stack is full"); 
+    
+} 
+} 
+void pop()
+{ 
+    if (top >= 0)
+    {
+        printf ("%d ", a[top]); 
+        top--; 
+        
+    } 
+    else
+{
+    printf ("Stack is empty");
+}
+}
+void main()
+{
+    int n, val;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter value: ");
+        scanf("%d", &val);
+        push(val);
+    }
+    printf("Elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        pop();
+    }
+}
